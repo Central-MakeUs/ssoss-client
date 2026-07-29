304622e4eae65d5d205f38e680015a04e751de68
안도현 <ahndohyeon@andohyeons-MacBook-Pro.local>
2026-07-30 01:31:05 +0900

    hotfix: 삭제된 탈퇴 페이지 복구

--- ssoss_flutter/lib/features/my_page/presentation/pages/settings/settings_components.dart
+++ ssoss_flutter/lib/features/my_page/presentation/pages/settings/settings_components.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:ssoss_flutter/common/widgets/text/app_text.dart';
 import 'package:ssoss_flutter/core/colors/app_colors.dart';
+import 'package:ssoss_flutter/core/constants/assets.dart';
 import 'package:ssoss_flutter/core/theme/app_text_styles.dart';
 
 class SettingsMenuGroup extends StatelessWidget {
@@ -51,6 +53,47 @@ class SettingsMenuItem extends StatelessWidget {
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsWithdrawButton extends StatelessWidget {
+  const SettingsWithdrawButton({
+    required this.onTap,
+    super.key,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      behavior: HitTestBehavior.opaque,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppText(
+              '탈퇴하기',
+              style: AppTextStyles.h6.copyWith(
+                color: AppColors.primary600,
+              ),
+            ),
+            const SizedBox(width: 6),
+            SvgPicture.asset(
+              AppAssets.icWithdraw,
+              width: 20,
+              height: 20,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primary600,
+                BlendMode.srcIn,
+              ),
+            ),
+          ],
         ),
       ),
     );
