7c951ee41b64bd3d2ff5fe720aa4dcfd9c0b79b3
안도현 <ahndohyeon@andohyeons-MacBook-Pro.local>
2026-07-31 03:27:11 +0900

    fix: 공용 텍스트필드 포커스 컬러 pressed 기반으로 변경

--- ssoss_flutter/lib/common/widgets/input/ssoss_text_field.dart
+++ ssoss_flutter/lib/common/widgets/input/ssoss_text_field.dart
@@ -6,7 +6,7 @@ import 'package:ssoss_flutter/core/colors/app_colors.dart';
 import 'package:ssoss_flutter/core/constants/assets.dart';
 import 'package:ssoss_flutter/core/theme/app_text_styles.dart';
 
-class SsossTextField extends StatelessWidget {
+class SsossTextField extends StatefulWidget {
   const SsossTextField({
     super.key,
     this.controller,
@@ -70,55 +70,98 @@ class SsossTextField extends StatelessWidget {
   final double? width;
   final double? height;
 
-  static const double defaultHeight = 44;
+  static const double _defaultHeight = 44;
   static const double _borderRadius = 8;
+  static const List<BoxShadow> _focusedShadow = [
+    BoxShadow(
+      color: Color(0x4DFF9E70),
+      blurRadius: 3,
+    ),
+  ];
+
+  @override
+  State<SsossTextField> createState() => _SsossTextFieldState();
+}
+
+class _SsossTextFieldState extends State<SsossTextField> {
+  late final FocusNode _focusNode;
+  late final bool _ownsFocusNode;
+  bool _hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _ownsFocusNode = widget.focusNode == null;
+    _focusNode = widget.focusNode ?? FocusNode();
+    _hasFocus = _focusNode.hasFocus;
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    if (_ownsFocusNode) {
+      _focusNode.dispose();
+    }
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    if (_hasFocus == _focusNode.hasFocus) {
+      return;
+    }
+    setState(() {
+      _hasFocus = _focusNode.hasFocus;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final resolvedTextColor = textColor ?? AppColors.neutral800;
-    final resolvedHintColor = hintColor ?? AppColors.neutral400;
-    final resolvedIconColor = searchIconColor ?? AppColors.neutral700;
-    final resolvedFillColor = enabled
-        ? fillColor ?? AppColors.white
-        : disabledFillColor ?? AppColors.neutral50;
+    final resolvedTextColor = widget.textColor ?? AppColors.neutral800;
+    final resolvedHintColor = widget.hintColor ?? AppColors.neutral400;
+    final resolvedIconColor = widget.searchIconColor ?? AppColors.neutral700;
+    final resolvedFillColor = widget.enabled
+        ? widget.fillColor ?? AppColors.white
+        : widget.disabledFillColor ?? AppColors.neutral50;
     final textStyle = AppTextStyles.b4;
-    final resolvedHeight = height ?? defaultHeight;
-    final resolvedErrorColor = errorBorderColor ?? AppColors.error500;
-    final resolvedBorderColor =
-        hasError ? resolvedErrorColor : borderColor ?? AppColors.neutral200;
-    final resolvedFocusedBorderColor = hasError
+    final resolvedHeight = widget.height ?? SsossTextField._defaultHeight;
+    final resolvedErrorColor = widget.errorBorderColor ?? AppColors.error500;
+    final resolvedBorderColor = widget.hasError
         ? resolvedErrorColor
-        : focusedBorderColor ?? AppColors.neutral600;
+        : widget.borderColor ?? AppColors.neutral200;
+    final resolvedFocusedBorderColor = widget.hasError
+        ? resolvedErrorColor
+        : widget.focusedBorderColor ?? AppColors.neutral600;
 
     final textField = TextField(
-      controller: controller,
-      focusNode: focusNode,
-      enabled: enabled,
-      readOnly: readOnly,
-      obscureText: multiline ? false : obscureText,
-      keyboardType:
-          keyboardType ?? (multiline ? TextInputType.multiline : null),
-      textInputAction:
-          textInputAction ?? (multiline ? TextInputAction.newline : null),
-      inputFormatters: inputFormatters,
-      onChanged: onChanged,
-      onSubmitted: onSubmitted,
+      controller: widget.controller,
+      focusNode: _focusNode,
+      enabled: widget.enabled,
+      readOnly: widget.readOnly,
+      obscureText: widget.multiline ? false : widget.obscureText,
+      keyboardType: widget.keyboardType ??
+          (widget.multiline ? TextInputType.multiline : null),
+      textInputAction: widget.textInputAction ??
+          (widget.multiline ? TextInputAction.newline : null),
+      inputFormatters: widget.inputFormatters,
+      onChanged: widget.onChanged,
+      onSubmitted: widget.onSubmitted,
       onTapOutside: (_) {
-        (focusNode ?? FocusManager.instance.primaryFocus)?.unfocus();
+        _focusNode.unfocus();
       },
-      minLines: multiline ? minLines : null,
-      maxLines: multiline ? maxLines : 1,
+      minLines: widget.multiline ? widget.minLines : null,
+      maxLines: widget.multiline ? widget.maxLines : 1,
       textAlignVertical:
-          multiline ? TextAlignVertical.top : TextAlignVertical.center,
-      cursorColor: hasError
+          widget.multiline ? TextAlignVertical.top : TextAlignVertical.center,
+      cursorColor: widget.hasError
           ? resolvedErrorColor
-          : focusedBorderColor ?? AppColors.primary400,
+          : widget.focusedBorderColor ?? AppColors.primary400,
       style: textStyle.copyWith(
         color: resolvedTextColor,
       ),
-      expands: expands,
+      expands: widget.expands,
       decoration: InputDecoration(
-        hintText: hintText,
+        hintText: widget.hintText,
         hintStyle: textStyle.copyWith(
           color: resolvedHintColor,
         ),
@@ -126,10 +169,11 @@ class SsossTextField extends StatelessWidget {
         fillColor: resolvedFillColor,
         isDense: true,
         contentPadding: EdgeInsets.symmetric(
-          horizontal: showSearchIcon || showLoadingIndicator ? 0 : 14,
+          horizontal:
+              widget.showSearchIcon || widget.showLoadingIndicator ? 0 : 14,
           vertical: 10,
         ),
-        prefixIcon: showSearchIcon
+        prefixIcon: widget.showSearchIcon
             ? Padding(
                 padding: const EdgeInsets.only(left: 14, right: 10),
                 child: SvgPicture.asset(
@@ -143,13 +187,13 @@ class SsossTextField extends StatelessWidget {
                 ),
               )
             : null,
-        prefixIconConstraints: showSearchIcon
+        prefixIconConstraints: widget.showSearchIcon
             ? const BoxConstraints(
                 minWidth: 48,
                 minHeight: 24,
               )
             : null,
-        suffixIcon: showLoadingIndicator
+        suffixIcon: widget.showLoadingIndicator
             ? const Padding(
                 padding: EdgeInsets.only(right: 14),
                 child: SizedBox(
@@ -162,7 +206,7 @@ class SsossTextField extends StatelessWidget {
                 ),
               )
             : null,
-        suffixIconConstraints: showLoadingIndicator
+        suffixIconConstraints: widget.showLoadingIndicator
             ? const BoxConstraints(
                 minWidth: 34,
                 minHeight: 20,
@@ -173,31 +217,38 @@ class SsossTextField extends StatelessWidget {
         focusedBorder: _border(resolvedFocusedBorderColor),
         errorBorder: _border(resolvedErrorColor),
         focusedErrorBorder: _border(resolvedErrorColor),
-        disabledBorder: _border(disabledBorderColor ?? AppColors.neutral200),
+        disabledBorder:
+            _border(widget.disabledBorderColor ?? AppColors.neutral200),
       ),
     );
 
-    if (multiline) {
-      return ConstrainedBox(
-        constraints: BoxConstraints(
-          minHeight: resolvedHeight,
-          minWidth: width ?? 0,
-          maxWidth: width ?? double.infinity,
-        ),
-        child: textField,
-      );
-    }
-
-    return SizedBox(
-      width: width,
-      height: resolvedHeight,
-      child: textField,
+    final field = DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(SsossTextField._borderRadius),
+        boxShadow: _hasFocus ? SsossTextField._focusedShadow : null,
+      ),
+      child: widget.multiline
+          ? ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: resolvedHeight,
+                minWidth: widget.width ?? 0,
+                maxWidth: widget.width ?? double.infinity,
+              ),
+              child: textField,
+            )
+          : SizedBox(
+              width: widget.width,
+              height: resolvedHeight,
+              child: textField,
+            ),
     );
+
+    return field;
   }
 
   OutlineInputBorder _border(Color color) {
     return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(_borderRadius),
+      borderRadius: BorderRadius.circular(SsossTextField._borderRadius),
       borderSide: BorderSide(color: color),
     );
   }
